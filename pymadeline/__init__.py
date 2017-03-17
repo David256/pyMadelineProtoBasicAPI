b634a415c2874f6a6497878822523f0b69ccbd8a
David <davidborja456@gmail.com>
2017-03-17 11:25:02 -0500

    new login method

--- pymadeline/__init__.py
+++ pymadeline/__init__.py
@@ -7,12 +7,11 @@ import json
 # The main module :D
 from pymadeline import worker
 
-
 class pyMadeline(object):
 	"""This class defines to a MadelineProto API methods
 	for Python.
 	"""
-	def __init__(self, token):
+	def __init__(self, token=None):
 		self.token = token
 
 	def push(self, method, **kwargs):
@@ -29,6 +28,64 @@ class pyMadeline(object):
 		)
 
 		return jresult
+
+	def login(self, step, **kwargs):
+		"""With this method, you can login in Telegram.
+
+		The step parameter indicate the process to do, It
+		can be 1, 2, 3 or 0. It will be 0 when you need to
+		create a new account. The step 1 is always required and
+		you must pass the parameter `phone` and Telegram will be
+		available to send you a verification code. The step 2
+		is normalty the last step, here you must pass the
+		parameter `code` to get the *permanent access token*.
+		But, if you have a password, then you must too use the
+		step 3 and pass the password with the parameter `password`.
+
+
+		step 1:
+		:param phone: you telephone number.
+
+		step 2:
+		:param code: a code sent to your telephone.
+
+
+		step 3:
+		:param password: the password.
+
+		step 0:
+		:param first_name: your first name.
+		:param last_name: (optional) your last name.
+		"""
+		if step == 1:
+			jresult = worker.request_login(kwargs['phone'])
+			self.token = jresult
+			return True
+
+		if step == 2:
+			jresult = self.push(
+				'completephonelogin',
+				code=kwargs['code']
+			)
+			self.token = jresult
+			return True
+
+		if step == 3:
+			jresult = self.push(
+				'complete2FALogin',
+				password=kwargs['password']
+			)
+			self.token = jresult
+			return True
+
+		if step == 0:
+			jresult = self.push(
+				'completesignup',
+				first_name=kwargs['first_name'],
+				last_name=kwargs['last_name'] if 'last_name' in kwargs else ''
+			)
+			self.token = jresult
+			return True
 
 	@staticmethod
 	def subobject(*args, **kwargs):
